f4828b2c8ddbe81c60f6e64ff0c79e406be44b20
mraiguo <810158465@qq.com>
2020-06-20 23:33:20 +0800

    使用MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
   entry: './src/index.js',
@@ -13,17 +13,13 @@ module.exports = {
       template: './src/index.html', // 模板路径
       hash: true // 在打包好的bundle.js 后加上hash
     }),
-    new ExtractTextWebpackPlugin('/style.less')
+    new MiniCssExtractPlugin({filename: 'css/index.less'})
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ExtractTextWebpackPlugin.extract({
-          // 将css用link的方式引入就不再需要style-loader了
-          fallback: "style-loader",
-          use: ['css-loader']
-        })
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   }
