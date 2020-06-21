cf3859d9144bdd6c92aca5dac587c0c198292471
mraiguo <810158465@qq.com>
2020-06-21 17:32:38 +0800

    尝试自定义webpack插件（报错： Error: Plugin could not be registered at 'html-webpack-plugin-before-html-processing'. Hook was not found.）

---
+++ MyPlugin.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// SetScriptTimestampPlugin.js
+class MyPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('SetScriptTimestampPlugin',
+      (compilation, callback) => {
+// 插件逻辑 调用compilation提供的plugin方法
+        compilation.plugin(
+          "html-webpack-plugin-before-html-processing",
+          function(htmlPluginData, callback) {
+// 读取并修改 script 上 src 列表
+            let jsScr = htmlPluginData.assets.js[0];
+            htmlPluginData.assets.js = [];
+            let result = `
+<script>
+let scriptDOM = document.createElement("script");
+let jsScr = "./${jsScr}";
+scriptDOM.src = jsScr + "?" + new Date().getTime();
+document.body.appendChild(scriptDOM)
+</script>
+`;
+            let resultHTML = htmlPluginData.html.replace(
+              "<!--MyPlugin inset script-->", result
+            );
+// 返回修改后的结果
+            htmlPluginData.html = "<script>console.log('my-plugin')</script>";
+          }
+        );
+      }
+    );
+  }
+}
+module.exports = MyPlugin;
--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,82 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin")
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MyPlugin = require('./MyPlugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve('dist')
+    filename: 'bundle.js',
+    path: path.resolve('dist'),
   },
   devtool: 'inline-source-map',
   plugins: [
-    new CleanWebpackPlugin(),
-    new ExtractTextWebpackPlugin('css/style.css'),
     new HtmlWebpackPlugin({
       template: './src/index.html', // 模板路径
-      hash: true // 在打包好的bundle.js 后加上hash
+      hash: true, // 在打包好的bundle.js 后加上hash
+      inject: true,
     }),
+    new MyPlugin({ options: true }),
+    new CleanWebpackPlugin(),
+    new ExtractTextWebpackPlugin('css/style.css'),
   ],
   module: {
     rules: [
       {
-        test: /\.scss$/,     // 解析scss
+        test: /\.scss$/, // 解析scss
         use: ExtractTextWebpackPlugin.extract({
           // 将css用link的方式引入就不再需要style-loader了
-          fallback: "style-loader",
-          use: ['css-loader', 'postcss-loader', 'sass-loader'] // 从右向左解析
-        })
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader'], // 从右向左解析
+        }),
       },
       {
-        test: /\.css$/,     // 解析css
+        test: /\.css$/, // 解析css
         use: ExtractTextWebpackPlugin.extract({
           // 将css用link的方式引入就不再需要style-loader了
-          fallback: "style-loader",
-          use: ['css-loader', 'postcss-loader']
-        })
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader'],
+        }),
       },
       {
-        test: /\.less$/,     // 解析less
+        test: /\.less$/, // 解析less
         use: ExtractTextWebpackPlugin.extract({
           // 将css用link的方式引入就不再需要style-loader了
-          fallback: "style-loader",
-          use: ['css-loader', 'postcss-loader', 'less-loader'] // 从右向左解析
-        })
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'less-loader'], // 从右向左解析
+        }),
       },
       {
         test: /\.(jpe?g|png|gif)$/,
         use: [{
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
-            limit: 8192,  // 小于8k的图片自动转成base64格式，并且不会存在实体图片
-            outputPath: 'images/' // 图片打包后存放的目录
-          }
-        }]
+            limit: 8192, // 小于8k的图片自动转成base64格式，并且不会存在实体图片
+            outputPath: 'images/', // 图片打包后存放的目录
+          },
+        }],
       },
       {
-        test: /\.js$/,
+        test: /\.js?x$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets:['@babel/preset-env']
-          }
+            presets: ['@babel/preset-env'],
+          },
         },
         include: /src/,
-        exclude: /node_modules/
-      }
+        exclude: /node_modules/,
+      },
     ],
   },
   resolve: {
     // 别名
     alias: {
-      pages:path.join(__dirname,'src/pages'),
-      component:path.join(__dirname,'src/component'),
+      pages: path.join(__dirname, 'src/pages'),
+      component: path.join(__dirname, 'src/component'),
     },
     // 省略后缀
-    extensions: ['.js', '.jsx', '.json', '.css', '.scss', '.less']
-  }
-}
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss', '.less'],
+  },
+};
